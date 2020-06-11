1c789168297e010c86e8754026a12e32ac34e2f7
Roberto Meneghelli <roberto.mng.89@gmail.com>
2020-06-11 16:32:45 -0300

    Versão 1.5
    Commit (Versão 1.5)

---
+++ Sql Server/Functions/Function to remove numbers of column.sql
@@ -0,0 +1,12 @@
+CREATE FUNCTION [dbo].[GetOnlyDigits](@Temp VarChar(1000))
+Returns VarChar(1000)
+AS
+Begin
+
+    Declare @KeepValues as varchar(50)
+    Set @KeepValues = '%[^0-9]%'
+    While PatIndex(@KeepValues, @Temp) > 0
+        Set @Temp = Stuff(@Temp, PatIndex(@KeepValues, @Temp), 1, '')
+
+    Return @Temp
+End
